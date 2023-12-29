97ec64cf7693c9222431cd7421b0807d4d160ae6
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-12-29 11:36:30 -0300

    Instalacion de Redux

--- APP/SW Guide/src/components/Cards.jsx
+++ APP/SW Guide/src/components/Cards.jsx
@@ -1,11 +1,17 @@
-
+import { useSelector } from "react-redux";
 import Card from "./Card";
 
 export default function Cards() {
+  const data = useSelector((state) => state.data);
+
   return (
-    <div>
-      <h1>Component Cards</h1>
-      <Card />
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-2xl font-bold mb-4">Component Cards</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {data.map((person) => (
+          <Card key={person.name} data={person} />
+        ))}
+      </div>
     </div>
   );
 }
